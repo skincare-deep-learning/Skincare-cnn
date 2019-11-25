9f4e13a98fd8f12c8178ed8bae8dde547fb24dec
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-25 12:35:28 +0000

    Add classifiers

--- cnn.py
+++ cnn.py
@@ -92,8 +92,8 @@ classifier.compile(optimizer=sgd, loss = 'categorical_crossentropy', metrics = [
 
 classifier.fit_generator( training_set,
     steps_per_epoch = 19118,
-    epochs = 25,
-    workers = 6,
+    epochs = 5,
+    workers = 8,
     validation_data = test_set,
     validation_steps = 4791)
 
